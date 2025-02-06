fc1bc3182c71d442a24167848af16d9c45c67261
bromscandium <kkmshbiu@protonmail.com>
2025-02-07 00:33:14 +0100

    function which called with commands

---
+++ commands.py
@@ -0,0 +1,73 @@
+from telegram import Update
+
+
+async def help(update: Update, context):
+    if update.message:
+        await update.message.reply_text(
+            'Citali ste kanal <a href="https://t.me/c/2307996875/4/52">ASAP</a>?',
+            parse_mode="HTML"
+        )
+
+
+async def rules(update: Update, context):
+    if update.message:
+        await update.message.reply_text(
+            'Citali ste kanal <a href="https://t.me/c/2307996875/4/52">ASAP</a>?',
+            parse_mode="HTML"
+        )
+
+
+async def moodle_passwords(update: Update, context):
+    if update.message:
+        await update.message.reply_text(
+            'Citali ste kanal <a href="https://t.me/c/2307996875/4/56">ASAP</a>?',
+            parse_mode="HTML"
+        )
+
+
+async def links(update: Update, context):
+    if update.message:
+        await update.message.reply_text(
+            'Citali ste kanal <a href="https://t.me/c/2307996875/4/57">ASAP</a>?',
+            parse_mode="HTML"
+        )
+
+
+async def scores(update: Update, context):
+    if update.message:
+        await update.message.reply_text(
+            'Citali ste kanal <a href="https://t.me/c/2307996875/4/63">ASAP</a>?',
+            parse_mode="HTML"
+        )
+
+
+async def schedule(update: Update, context):
+    if update.message:
+        await update.message.reply_text(
+            'Citali ste kanal <a href="https://t.me/c/2307996875/4/67">ASAP</a>?',
+            parse_mode="HTML"
+        )
+
+
+async def map_tuke(update: Update, context):
+    if update.message:
+        await update.message.reply_text(
+            'Citali ste kanal <a href="https://t.me/c/2307996875/4/68">ASAP</a>?',
+            parse_mode="HTML"
+        )
+
+
+async def map_5p(update: Update, context):
+    if update.message:
+        await update.message.reply_text(
+            'Citali ste kanal <a href="https://t.me/c/2307996875/4/69">ASAP</a>?',
+            parse_mode="HTML"
+        )
+
+
+async def studijne(update: Update, context):
+    if update.message:
+        await update.message.reply_text(
+            'Citali ste kanal <a href="https://t.me/c/2307996875/4/72">ASAP</a>?',
+            parse_mode="HTML"
+        )
