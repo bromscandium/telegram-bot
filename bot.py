3a1f691d5f3098faa0c71892ed1b3a01d45eaab5
bromscandium <kkmshbiu@protonmail.com>
2025-02-07 00:33:30 +0100

    main

--- bot.py
+++ bot.py
@@ -1,15 +1,29 @@
-from telegram.ext import ApplicationBuilder
-from handlers import start
+from telegram.ext import CommandHandler, ApplicationBuilder, MessageHandler, filters
+from commands import *
 from config import TOKEN
+from interactions import (
+    welcome,
+)
+
 
 def main():
-    app = ApplicationBuilder().token(TOKEN).build()
+    bot = ApplicationBuilder().token(TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start))
+    bot.add_handler(CommandHandler("help", help))
+    bot.add_handler(CommandHandler("schedule", schedule))
+    bot.add_handler(CommandHandler("rules", rules))
+    bot.add_handler(CommandHandler("moodle_passwords", moodle_passwords))
+    bot.add_handler(CommandHandler("links", links))
+    bot.add_handler(CommandHandler("scores", scores))
+    bot.add_handler(CommandHandler("map_tuke", map_tuke))
+    bot.add_handler(CommandHandler("map_5p", map_5p))
+    bot.add_handler(CommandHandler("studijne", studijne))
+
+    bot.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, welcome))
 
     print("Starting Telegram Bot...")
-    app.run_polling()
+    bot.run_polling()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
