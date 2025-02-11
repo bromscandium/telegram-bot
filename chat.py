8a95605576bb0155034eef5bfc09d7f888113abb
bromscandium <kkmshbiu@protonmail.com>
2025-02-11 02:01:12 +0100

    optimized forwarding messages

--- chat.py
+++ chat.py
@@ -6,39 +6,17 @@ user_state = {}
 
 
 async def start(update: Update, context):
-    user_id = update.message.from_user.id
-
-    if update.message.chat.type == "private":
+    if update.message.chat.type == 'private':
+        user_id = update.message.from_user.id
         user_state[user_id] = 'waiting_for_message'
-        await update.message.reply_text("Привіт! Напишіть ваше повідомлення, і я відправлю його в групу.")
+        await update.message.reply_text("Напишіть ваше повідомлення, і я відправлю його в адміністраторам")
 
 
-async def handle_message(update: Update, context):
-    chat_type = update.message.chat.type
-    if chat_type != 'private':
-        return
-
-    user_id = update.message.from_user.id
-    username = update.message.from_user.username
-    fullname = update.message.from_user.full_name
-
-    if user_id in user_state and user_state[user_id] == 'waiting_for_message':
-        text_to_send = ""
-        if update.message.text is not None:
-            text_to_send = update.message.text if update.message.text else ""
-        elif update.message.caption is not None:
-            text_to_send = update.message.caption if update.message.caption else ""
-        text_to_send += f"\n\n#пишутьгенчі\nid{user_id}\n@{username}\n{fullname}"
-        if update.message.text:
-            await context.bot.send_message(chat_id=ADMIN_CHAT_ID, text=text_to_send)
-
-        if update.message.photo:
-            await context.bot.send_photo(chat_id=ADMIN_CHAT_ID, photo=update.message.photo[-1].file_id,
-                                         caption=text_to_send)
-        elif update.message.video:
-            await context.bot.send_video(chat_id=ADMIN_CHAT_ID, video=update.message.video.file_id,
-                                         caption=text_to_send)
+async def start_message(update: Update, context):
+    if update.message.chat.type == 'private' and user_state.get(update.message.from_user.id) == 'waiting_for_message':
+        await context.bot.forward_message(chat_id=ADMIN_CHAT_ID, from_chat_id=update.message.chat.id,
+                                          message_id=update.message.message_id)
 
         await update.message.reply_text("Ваше повідомлення було надіслано в групу.")
 
-        user_state[user_id] = 'message_sent'
+        user_state[update.message.from_user.id] = 'message_sent'
