bf9306659c8c8dd43c379d3ebe4343130eab75ff
bromscandium <77798518+bromscandium@users.noreply.github.com>
2025-02-11 02:00:02 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = 'your_token'
+CHAT_ID = 'your_group_id'
+ADMIN_CHAT_ID = 'your_admin_id'
+ALLOWED_IDS = [your_group_id, your_admin_id]
+REACTIONS = ["🔥", "👍", "🤮", "😢", "😁"]
+MAX_USAGE = 2
+RESET_TIME_SECONDS = 300
