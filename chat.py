919a66c6f1f33ce5df4aa8e3e2bb27e2d17d0dce
bromscandium <kkmshbiu@protonmail.com>
2025-02-12 19:11:43 +0100

    more info about sender

--- chat.py
+++ chat.py
@@ -16,7 +16,9 @@ async def start_message(update: Update, context):
     if update.message.chat.type == 'private' and user_state.get(update.message.from_user.id) == 'waiting_for_message':
         await context.bot.forward_message(chat_id=ADMIN_CHAT_ID, from_chat_id=update.message.chat.id,
                                           message_id=update.message.message_id)
-
+        await context.bot.send_message(chat_id=ADMIN_CHAT_ID,
+                                       text=f"\n\n#надіслали_в_бота\n<blockquote>id{update.message.from_user.id}\n@{update.message.from_user.username}\n{update.message.from_user.full_name}</blockquote>",
+                                       parse_mode="HTML")
         await update.message.reply_text("Ваше повідомлення було надіслано в групу.")
 
         user_state[update.message.from_user.id] = 'message_sent'
