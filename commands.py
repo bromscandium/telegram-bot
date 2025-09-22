6de02fcfd386c41ae8bf1db77c98d9e1bf7b764e
User <yaroslav.yeromenko@student.tuke.sk>
2025-09-22 23:04:30 +0200

    fixed commands (nw)

--- commands.py
+++ commands.py
@@ -60,97 +60,88 @@ def personal_limit_usage(seconds):
 
 @limit_usage
 async def rules(update: Update, context):
-    if update.message:
-        await update.message.reply_text(
-            'čítali ste kanál <a href="https://t.me/c/2307996875/4/224677">ASAP</a>?',
-            parse_mode="HTML"
-        )
+    await update.message.reply_text(
+        'čítali ste kanál <a href="https://t.me/c/2307996875/4/224677">ASAP</a>?',
+        parse_mode="HTML"
+    )
 
 
 @limit_usage
 async def moodle(update: Update, context):
-    if update.message:
-        await context.bot.forward_message(
-            chat_id=update.effective_chat.id,
-            from_chat_id=CHAT_ID,
-            message_id=224679,
-            message_thread_id=update.message.message_thread_id,
-        )
+    await context.bot.forward_message(
+        chat_id=update.effective_chat.id,
+        from_chat_id=CHAT_ID,
+        message_id=224679,
+        message_thread_id=update.message.message_thread_id,
+    )
 
 
 @limit_usage
 async def links(update: Update, context):
-    if update.message:
-        await context.bot.forward_message(
-            chat_id=update.effective_chat.id,
-            from_chat_id=CHAT_ID,
-            message_id=224678,
-            message_thread_id=update.message.message_thread_id,
-        )
+    await context.bot.forward_message(
+        chat_id=update.effective_chat.id,
+        from_chat_id=CHAT_ID,
+        message_id=224678,
+        message_thread_id=update.message.message_thread_id,
+    )
 
 
 @limit_usage
 async def scores(update: Update, context):
-    if update.message:
-        await update.message.reply_text(
-            'Čítali ste kanál <a href="https://t.me/c/2307996875/4/224680">ASAP</a>?',
-            parse_mode="HTML"
-        )
+    await update.message.reply_text(
+        'Čítali ste kanál <a href="https://t.me/c/2307996875/4/224680">ASAP</a>?',
+        parse_mode="HTML"
+    )
 
 
 @limit_usage
 async def plan(update: Update, context):
-    if update.message:
-        await context.bot.forward_message(
-            chat_id=update.effective_chat.id,
-            from_chat_id=CHAT_ID,
-            message_id=224681,
-            message_thread_id=update.message.message_thread_id
-        )
+    await context.bot.forward_message(
+        chat_id=update.effective_chat.id,
+        from_chat_id=CHAT_ID,
+        message_id=224681,
+        message_thread_id=update.message.message_thread_id
+    )
 
 
 @limit_usage
 async def maptuke(update: Update, context):
-    if update.message:
-        await context.bot.forward_message(
-            chat_id=update.effective_chat.id,
-            from_chat_id=CHAT_ID,
-            message_id=224682,
-            message_thread_id=update.message.message_thread_id
-        )
+    await context.bot.forward_message(
+        chat_id=update.effective_chat.id,
+        from_chat_id=CHAT_ID,
+        message_id=224682,
+        message_thread_id=update.message.message_thread_id
+    )
 
 
 @limit_usage
 async def map5p(update: Update, context):
-    if update.message:
-        await context.bot.forward_message(
-            chat_id=update.effective_chat.id,
-            from_chat_id=CHAT_ID,
-            message_id=224683,
-            message_thread_id=update.message.message_thread_id
-        )
+    await context.bot.forward_message(
+        chat_id=update.effective_chat.id,
+        from_chat_id=CHAT_ID,
+        message_id=224683,
+        message_thread_id=update.message.message_thread_id
+    )
 
 
 @limit_usage
 async def studijne(update: Update, context):
-    if update.message:
-        await context.bot.forward_message(
-            chat_id=update.effective_chat.id,
-            from_chat_id=CHAT_ID,
-            message_id=224684,
-            message_thread_id=update.message.message_thread_id
-        )
+    await context.bot.forward_message(
+        chat_id=update.effective_chat.id,
+        from_chat_id=CHAT_ID,
+        message_id=224684,
+        message_thread_id=update.message.message_thread_id
+    )
 
 
 @limit_usage
 async def schedule(update: Update, context):
-    if update.message:
-        await context.bot.forward_message(
-            chat_id=update.effective_chat.id,
-            from_chat_id=CHAT_ID,
-            message_id=224685,
-            message_thread_id=update.message.message_thread_id
-        )
+    await context.bot.forward_message(
+        chat_id=update.effective_chat.id,
+        from_chat_id=CHAT_ID,
+        message_id=224685,
+        message_thread_id=update.message.message_thread_id
+    )
 
 
 @limit_usage
